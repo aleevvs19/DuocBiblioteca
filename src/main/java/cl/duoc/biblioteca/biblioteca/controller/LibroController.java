739a8ce4cbf56c4924e0bbecae8669659436b47d
aleevvs19 <ale.salazarv@duocuc.cl>
2026-03-27 17:00:32 -0300

    act nv

---
+++ src/main/java/cl/duoc/biblioteca/biblioteca/controller/LibroController.java
@@ -0,0 +1,42 @@
+package cl.duoc.biblioteca.biblioteca.controller;
+
+import cl.duoc.biblioteca.biblioteca.model.Libro;
+import cl.duoc.biblioteca.biblioteca.service.LibroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/libros")
+public class LibroController {
+
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping
+    public List<Libro> listarLibros() {
+        return libroService.getLibros();
+    }
+
+    @PostMapping
+    public Libro agregarLibro(@RequestBody Libro libro) {
+        return libroService.saveLibro(libro);
+    }
+
+    @GetMapping("/{id}")
+    public Libro buscarLibro(@PathVariable int id) {
+        return libroService.getLibroId(id);
+    }
+
+    @PutMapping("/{id}")
+    public Libro actualizarLibro(@PathVariable int id, @RequestBody Libro libro) {
+        libro.setId(id);
+        return libroService.updateLibro(libro);
+    }
+
+    @DeleteMapping("/{id}")
+    public String eliminarLibro(@PathVariable int id) {
+        return libroService.deleteLibro(id);
+    }
+}
